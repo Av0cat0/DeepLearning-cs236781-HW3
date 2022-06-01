dc631ccba19b08e5aaaded7161dbca9128a68be9
roni <roni.ronen.roni@gmail.com>
2022-06-01 03:14:56 +0300

    Initial commit

---
+++ hw3/answers.py
@@ -0,0 +1,209 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+# ==============
+# Part 1 answers
+
+part1_q1 = r"""
+**Your answer:**
+
+1.
+
+a. 
+According to tutorial 5, the Jacobian tensor $\pderiv{\mat{Y}}{\mat{X}}$ defined as a 4 dimension matrix: $(\pderiv{\mat{Y}}{\mat{X}})_{i,j,k,l}$ = $\pderiv{\mat{Y}_{i,j}}{\mat{X}_{k,l}}$ 
+and because we know that ${X}$ has the shape of (64,1024) (samples x input dimension) and ${Y}$ has the shape of (64,512) (samples x output dimension), we an conclude that the VJP has the shape of (64,512,64,1024)
+
+b.
+The VJP is a sparse matrix because for every output ${Y_i}$ the only relevant input is ${X_i}$ because the VJP is the output of the layer w.r.t. the input so only the input that produces the specific output ${Y_i}$ has a value of $\pderiv{\mat{Y}_{i}}{\mat{X}_{j}}$ 
+we can describe the VJP matrix as the following:
+If we look only on the dimensions (of the matrix) which represent the input/output dimensions, the matrix will be diagonal but because we have another 2 dimensions which represent the number of samples, the structure of the matrix will be duplicated in accordance so the VJP matrix is diagonal in 2 dimensions and duplicated in the other 2 dimensions.
+
+c.
+"""
+
+part1_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+# Part 2 answers
+
+
+def part2_overfit_hp():
+    wstd, lr, reg = 0, 0, 0
+    # TODO: Tweak the hyperparameters until you overfit the small dataset.
+    # ====== YOUR CODE: ======
+        wstd = 0.1
+    lr = 0.1
+    reg = 0.01
+    # ========================
+    return dict(wstd=wstd, lr=lr, reg=reg)
+
+
+def part2_optim_hp():
+    wstd, lr_vanilla, lr_momentum, lr_rmsprop, reg, = 0, 0, 0, 0, 0
+
+    # TODO: Tweak the hyperparameters to get the best results you can.
+    # You may want to use different learning rates for each optimizer.
+    # ====== YOUR CODE: ======
+    wstd = 0.1
+    lr_vanilla = 0.03
+    lr_momentum = 0.004
+    lr_rmsprop = 0.0001
+    reg = 0.001
+    # ========================
+    return dict(
+        wstd=wstd,
+        lr_vanilla=lr_vanilla,
+        lr_momentum=lr_momentum,
+        lr_rmsprop=lr_rmsprop,
+        reg=reg,
+    )
+
+
+def part2_dropout_hp():
+    wstd, lr, = 0, 0
+    # TODO: Tweak the hyperparameters to get the model to overfit without
+    # dropout.
+    # ====== YOUR CODE: ======
+    wstd, lr, = 0.01, 0.001
+    # ========================
+    return dict(wstd=wstd, lr=lr)
+
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
+
+# ==============
+# Part 3 answers
+
+
+def part3_rnn_hyperparams():
+    hypers = dict(
+        batch_size=0,
+        seq_len=0,
+        h_dim=0,
+        n_layers=0,
+        dropout=0,
+        learn_rate=0.0,
+        lr_sched_factor=0.0,
+        lr_sched_patience=0,
+    )
+    # TODO: Set the hyperparameters to train the model.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return hypers
+
+
+def part3_generation_params():
+    start_seq = ""
+    temperature = 0.0001
+    # TODO: Tweak the parameters to generate a literary masterpiece.
+    # ====== YOUR CODE: ======
+    raise NotImplementedError()
+    # ========================
+    return start_seq, temperature
+
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q4 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+# ==============
